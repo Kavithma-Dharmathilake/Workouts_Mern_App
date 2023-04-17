cec142aeacd07c8f83b7adc0211a98d2d5cf0fce
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2023-04-17 12:22:45 +0530

    Display all details with nav bar

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+<header>
+    <div className="container">
+        <Link to='/'>
+        <h1>Workout Buddy</h1>
+
+        </Link>
+
+
+    </div>
+</header>
+
+
+      );
+}
+ 
+export default NavBar;
---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,13 @@
+const WorkooutDetails = ({ workout }) => {
+    return (
+
+        <div className="workout-details">
+            <h4>{workout.title}</h4>
+            <p><strong>Load (kg) : </strong>{workout.load}</p>
+            <p><strong>Reps : </strong>{workout.reps}</p>
+
+        </div>
+    );
+}
+
+export default WorkooutDetails;
